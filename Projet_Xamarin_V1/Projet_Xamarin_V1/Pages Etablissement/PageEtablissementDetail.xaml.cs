ebe7709c3df1c1b62111ec50d5764123b5793976
Logan BRISY <Logan.BRISY@student.hel.be>
2021-01-05 15:14:47 +0100

    Ajout de la vue sur la carte

--- Projet_Xamarin_V1/Projet_Xamarin_V1/Pages Etablissement/PageEtablissementDetail.xaml.cs
+++ Projet_Xamarin_V1/Projet_Xamarin_V1/Pages Etablissement/PageEtablissementDetail.xaml.cs
@@ -61,5 +61,19 @@ namespace Projet_Xamarin_V1
             remplirLvAvisEtablissement();
             lvAvisEtablissement.EndRefresh();
         }
+
+        private async void btnVueMaps_Clicked(object sender, EventArgs e)
+        {
+            var placemark = new Placemark
+            {
+                CountryName = ePays.Text,
+                AdminArea = eCodePostal.Text,
+                Thoroughfare = eNom.Text + " " + eNumero.Text,
+                Locality = eVille.Text
+            };
+            var options = new MapLaunchOptions { Name = eNom.Text + " " + eNumero.Text/*, NavigationMode = NavigationMode.Driving*/ };
+
+            await Map.OpenAsync(placemark, options);
+        }
     }
 }
